a55ba186cc680310a9113b6c32df014f60b5d985
shivanisawant312 <59473179+shivanisawant312@users.noreply.github.com>
2020-01-03 17:28:39 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pickle
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+@app.route('/predict',methods = ['POST'])
+def get_result():
+    poly = pickle.load(open('Poly.pkl','rb'))
+    model = pickle.load(open('model.pkl', 'rb'))
+    query = [[float(request.form['text2'])]]
+    x_query = poly.transform(query)
+    sal=model.predict(x_query)
+    #return "hii"
+    return 'Dear'+request.form["text1"]+'Your Predicted salary'+ request.form["text2"]+'Experience is :'+str(sal)
+if __name__ == '__main__':
+   app.run(debug = True)
